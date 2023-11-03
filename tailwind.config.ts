09c2c097bec675d54091a80779fad2460989423c
Bermon Painter <git@bermonpainter.com>
2023-11-03 09:54:02 -0400

    Added Tailwind config styles, added basic Mongo config.

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -8,6 +8,60 @@ const config: Config = {
   ],
   theme: {
     extend: {
+      colors: {
+        primary: {
+          base: '#002E5D', // BYU Blue
+          light: '#3E5B99', // Lighter shade of BYU Blue
+          dark: '#001F40', // Darker shade of BYU Blue
+        },
+        secondary: {
+          base: '#FFFFFF', // White for contrast
+          light: '#F2F2F2', // Light gray as secondary light
+          dark: '#D1D1D1', // Dark gray as secondary dark
+        },
+        gray: {
+          lightest: '#F7F7F7',
+          lighter: '#E1E1E1',
+          light: '#CFCFCF',
+          base: '#B1B1B1',
+          dark: '#939393',
+          darker: '#757575',
+          darkest: '#575757',
+        },
+        alert: {
+          info: '#1E90FF',
+          success: '#28A745',
+          warning: '#FFC107',
+          danger: '#DC3545',
+        },
+      },
+      screens: {
+        sm: '640px',
+        m: '768px',
+        l: '1024px',
+        xl: '1280px',
+        xxl: '1536px',
+      },
+      fontFamily: {
+        heading: ['Georgia', 'serif'],
+        body: ['Arial', 'sans-serif'],
+      },
+      spacing: {
+        xs: '0.25rem', // 1 * 0.25 = 0.25rem (4px)
+        s: '0.5rem',   // 2 * 0.25 = 0.5rem (8px)
+        m: '1rem',     // 4 * 0.25 = 1rem (16px)
+        l: '2rem',     // 8 * 0.25 = 2rem (32px)
+        xl: '4rem',    // 16 * 0.25 = 4rem (64px)
+        xxl: '8rem',   // 32 * 0.25 = 8rem (128px)
+      },
+      borderRadius: {
+        none: '0',
+        sm: '0.125rem',
+        basd: '0.25rem',
+        m: '0.375rem',
+        l: '0.5rem',
+        full: '9999px',
+      },
       backgroundImage: {
         'gradient-radial': 'radial-gradient(var(--tw-gradient-stops))',
         'gradient-conic':
