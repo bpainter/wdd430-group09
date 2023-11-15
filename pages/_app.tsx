0b9fc94cee4009004b95cd37b46a542f89ec82b2
Bermon Painter <git@bermonpainter.com>
2023-11-15 10:11:51 -0500

    Fixed layout bug with NextAuth in the _app file

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,4 +1,5 @@
 import type { AppProps } from 'next/app';
+import { SessionProvider } from "next-auth/react";
 import '../styles/globals.css';
 import Navigation from '../components/layout/Navigation';
 import Footer from '../components/layout/Footer';
@@ -6,9 +7,11 @@ import Footer from '../components/layout/Footer';
 function HandcraftedHaven({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navigation />
-      <Component {...pageProps} />
-      <Footer />
+      <SessionProvider session={pageProps.session}>
+        <Navigation />
+        <Component {...pageProps} />
+        <Footer />
+      </SessionProvider>    
     </>
   );
 }
