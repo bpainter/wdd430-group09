b9fddc9c2898a350ca883299513f97729f3f7bbd
Bermon Painter <git@bermonpainter.com>
2023-11-27 06:45:44 -0600

    Fixed but with displaying product categories

--- pages/products/index.tsx
+++ pages/products/index.tsx
@@ -42,12 +42,12 @@ export default function Products({ products }: ProfilesProps) {
       <div className="mx-auto max-w-2xl px-4 py-16 sm:px-6 sm:py-24 lg:max-w-7xl lg:px-8">
         <h2 className="text-2xl font-bold tracking-tight text-gray-900">All products</h2>
 
-        <div className="mt-6 grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-4 xl:gap-x-8">
+        <div className="grid grid-cols-1 gap-x-6 gap-y-10 sm:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 xl:gap-x-8 mt-4">
           {/* ... TailwindUI Product List Component ... */}
           {products.map((product) => (
             <div key={product._id} className="group relative">
               {/* Product Card Content Here */}
-              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-md bg-gray-200 lg:aspect-none group-hover:opacity-75 lg:h-80">
+              <div className="aspect-h-1 aspect-w-1 w-full overflow-hidden rounded-lg bg-gray-200 xl:aspect-h-8 xl:aspect-w-7">
                 <Image
                   src={product.images[0]}
                   alt={product.title}
@@ -55,17 +55,22 @@ export default function Products({ products }: ProfilesProps) {
                   objectFit="cover"
                   className="w-full h-full object-center object-cover lg:w-full lg:h-full overflow-hidden rounded-md" />
               </div>
-              <div className="mt-4 flex justify-between">
+              <div className="flex justify-between mt-2">
                 <div>
                   <h3 className="text-sm text-gray-700">
-                    <Link className="absolute inset-0" href={`/products/${product._id}`}>
+                    <Link href={`/products/${product._id}`}>
+                      <span aria-hidden="true" className="absolute inset-0"></span>
                       {product.title}
                     </Link>
                   </h3>
-                  <p className="mt-1 text-sm text-gray-500">{product.title}</p>
                 </div>
                 <p className="text-sm font-medium text-gray-900">${product.price}</p>
               </div>
+              <p className="text-sm text-gray-500">
+                {product.categories.map((category, index) => (
+                  <span key={index}>{category}</span>
+                ))}
+              </p>
             </div>
           ))}
         </div>
